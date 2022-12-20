08fbee62ca141bf5af8f0813649f37e80c533da0
0xdeenz <0xdeenz@protonmail.com>
2022-12-20 10:28:39 +0100

    fix dependencies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 declare module '@metamask/jazzicon'
+declare module "@web3-react/core"
 declare module '@web3-react/walletconnect-connector'
 declare module '@web3-react/walletlink-connector'
 declare module '@chakra-ui/react'
